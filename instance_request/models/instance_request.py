ea39790262b788fc0d7200e71007d6f8c79e6c7a
assabepolo <p.assabe@karizma.ma>
2023-01-30 11:35:46 +0100

    [FIX] Seance vue et bouttons

--- instance_request/models/instance_request.py
+++ instance_request/models/instance_request.py
@@ -9,3 +9,33 @@ class InstanceRequest(models.Model):
 
     name = fields.Char(string="Designation")
     limit_date = fields.Date(string="Date limite de traitement")
+    state = fields.Selection(
+        [
+            ("draft", "Draft"),
+            ("submitted", "Submitted"),
+            ("in_progress", "In progress"),
+            ("done", "Done"),
+        ],
+        default="draft",
+    )
+    color = fields.Selection(
+        [('red', "Red"), ('green', "Green"), ('yellow', "Yellow")], default='red'
+    )
+
+    def action_draft(self):
+        for record in self:
+            record.state = 'draft'
+
+    def action_submit(self):
+        for record in self:
+            get_if_submit = self.env.context.get('get_if_submit', False)
+            print("===============>", get_if_submit)
+            record.state = 'submitted'
+
+    def action_progress(self):
+        for record in self:
+            record.state = 'in_progress'
+
+    def action_done(self):
+        for record in self:
+            record.state = 'done'
